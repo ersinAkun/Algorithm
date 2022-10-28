9406a2904638d3c75f26647cafd417d26a290c75
Ersin Akun <ersinakun34@gmail.com>
2022-10-28 22:32:55 +0300

    mergeSortlast

--- src/sorting_Algorithms/mergeSort/MergeSort.java
+++ src/sorting_Algorithms/mergeSort/MergeSort.java
@@ -1,67 +1,75 @@
 package sorting_Algorithms.mergeSort;
 
 public class MergeSort {
-    public static void merge(int[] arr, int left, int middle, int right){
-        // sol taraftaki alt-array'in uzunlugu
-        int low = middle - left + 1;
-        // sagdaki alt-array'in uzunlugu
-        int high = right - middle;
+    public static void merge(int arr[], int left, int middle, int right){
 
-        // sol ve sagdki alt arrayler olusturuluyor
-        int L[] = new int[low];   // sol alt array
-        int R[] = new int[high];  // sag alt array
+        int low = middle - left + 1; //sol alt-array büğüklüğü
+        int high = right - middle;//sağ alt-array büğüklüğü
 
-        int i=0,j=0;
-        //elemanlari soldaki alt arraye kopyaliyoruz
-        for ( i = 0; i < low; i++) {
-            L[i]=arr[left+1];
+        int L[] = new int[low];//sol-sağ alt arrayler oluşturuluyor
+        int R[] = new int[high];
+
+        int i = 0, j = 0;
+
+        for (i = 0; i < low; i++)//sol alt arraya elemanlar kopyalanıyor
+        {
+            L[i] = arr[left + i];
         }
-        //elemanlari sagdaki alt arraye kopyaliyoruz
-        for ( j = 0; j < high; j++) {
-            R[j]=arr[middle+1+j];
+        for (j = 0; j < high; j++)//sağ alt arraya elemanlar kopyalanıyor
+        {
+            R[j] = arr[middle + 1 + j];
         }
+        int k = left;//
+        i = 0;//
+        j = 0;
 
-        int k = left;  //sortlamaya baslanacak index
-        i=0;  // merge oncesi variable sifirlama!!
-        j=0;
+        while (i < low && j < high){ //sol-sağ alt arrayler merge leniyor
 
-        while (i>low && j<high){   //sol-sag alt array'ler merge 'leniyor
-            if (L[i]<=R[j]){
-                arr[k]=L[i];
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
                 i++;
-            }else {
-                arr[k]=R[j];
+            }
+            else{
+                arr[k] = R[j];
                 j++;
             }
             k++;
         }
-        while (i<low){  // sol alt array de kalan elemanlar merge leniyor
-            arr[k]=L[i];
+
+        while (i < low){ //sol arrayde kalan elemanlar merge leniyor
+            arr[k] = L[i];
             i++;
             k++;
         }
-        while (j<low) {  // sag alt array de kalan elemanlar merge leniyor
+
+        while (j < high){//sağ arrayde kalan elemanlar merge leniyor
             arr[k] = R[j];
             j++;
             k++;
         }
-
     }
 
 
-    public static void mergeSort(int[] arr, int left, int right){
-        int middle ;
-        if (left<right){
-            middle = (left+right)/2;
-            mergeSort(arr,left,middle); // sol alt array\
-            mergeSort(arr,middle+1,right);
+    public static void mergeSort(int arr[], int left, int right) {
 
-            merge(arr,left,middle,right);  //sol ve sag alt arrayler merge'leniyor
+        int middle;
+
+        if (left < right) {
+            middle = (left + right) / 2;
+
+            mergeSort(arr, left, middle);//sol alt-array
+
+            mergeSort(arr, middle + 1, right);//sağ alt-array
+
+
+
+            merge(arr, left, middle, right);//2 array birleştiriliyor
         }
-
-
-
     }
+
+
+
 
 
 }
